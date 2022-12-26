4b6c88e44bf4ca009aa0fd43941418c510412921
mutuajames <njorog3.james@gmail.com>
2022-12-26 14:31:14 +0300

    Channel layer

--- chat/settings.py
+++ chat/settings.py
@@ -59,9 +59,6 @@ TEMPLATES = [
     },
 ]
 
-# Daphne
-ASGI_APPLICATION = "mysite.asgi.application"
-
 WSGI_APPLICATION = 'chat.wsgi.application'
 
 
@@ -118,3 +115,17 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Daphne
+ASGI_APPLICATION = "chat.asgi.application"
+
+#Channels
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+        },
+    },
+}
